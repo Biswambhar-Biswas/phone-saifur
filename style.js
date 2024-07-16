85940ec9f82379403524a3c1d61428235361927f
Biswambhar Biswas <biswasbony8967@gmail.com>
2024-07-16 17:42:52 +0530

    Initial commit

---
+++ style.js
@@ -0,0 +1,13 @@
+
+    function hide(){
+    //    let ion = document.getElementById('ion');
+    //    let ioon = document.getElementById('ioon');
+    //    let body = document.getElementById('body');
+       if(ioon.style.display != 'none' ){
+        ioon.style.display = 'none';
+       }
+       else{
+        ioon.style.display = 'block';
+       }
+    }
+
